51ad9d308f1d800d91a94878185bc8b54379c344
t.filipova13@gmail.com <t.filipova13@gmail.com>
2018-10-19 17:24:35 +0300

    Spring created

---
+++ Spring/src/main/java/com/mystique/springtruckercard/SpringtruckercardApplication.java
@@ -0,0 +1,12 @@
+package com.mystique.springtruckercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtruckercardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtruckercardApplication.class, args);
+    }
+}
