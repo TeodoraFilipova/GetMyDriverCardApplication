caccb62cea3a79b3974abf36feb77ab4fd72e24c
t.filipova13@gmail.com <t.filipova13@gmail.com>
2018-10-22 16:17:56 +0300

    updated model CardApplicationForm with Hibernate annotations

--- Spring/src/main/java/com/mystique/springdrivercard/models/CardApplicationForm.java
+++ Spring/src/main/java/com/mystique/springdrivercard/models/CardApplicationForm.java
@@ -1,34 +1,35 @@
 package com.mystique.springdrivercard.models;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "cardapplicationforms")
 public class CardApplicationForm {
-    //table "applications"
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ApplicationID")
     private int applicationId;
-    //table "trucker"
-    private int driverId;
-    private String personalNumber;
-    private String firstName;
-    private String lastName;
-    private Date dateOfBirth;
-    private String driverAddress;
-    private String phoneNumber;
-    private String driverEmail;
-    private int driverSelfieId;
-    private int drivingPicId;
-    //table "applications"
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DriverID")
+    private Driver driver;
+
+    @Column(name = "DateOfSubmission")
     private Date dateOfSubmission;
+
+    @Column(name = "Status")
     private String status;
+
+    @Column(name = "Type")
     private String type;
+
+    @Column(name = "PaymentInformation")
     private String paymentInformation;
-    private int signaturePicId;
-    //table "pictures"
-    private byte[] driverSelfie;
-    private byte[] drivingPic;
-    private byte[] signaturePic;
 
-
-
+    @OneToOne
+    @JoinColumn(name = "SignaturePicID")
+    private Picture signaturePicture;
 
     // methods for changing status in Repository
     public String getStatus() {
@@ -37,5 +38,53 @@ public class CardApplicationForm {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public int getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(int applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Date getDateOfSubmission() {
+        return dateOfSubmission;
+    }
+
+    public void setDateOfSubmission(Date dateOfSubmission) {
+        this.dateOfSubmission = dateOfSubmission;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPaymentInformation() {
+        return paymentInformation;
+    }
+
+    public void setPaymentInformation(String paymentInformation) {
+        this.paymentInformation = paymentInformation;
+    }
+
+    public Picture getSignaturePicture() {
+        return signaturePicture;
+    }
+
+    public void setSignaturePicture(Picture signaturePicture) {
+        this.signaturePicture = signaturePicture;
     }
 }
