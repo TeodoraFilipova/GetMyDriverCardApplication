ea5759d03459f873986913c86750f2ac86c0b036
t.filipova13@gmail.com <t.filipova13@gmail.com>
2018-10-29 16:09:40 +0200

    added functionality for parsing json objects

---
+++ Android/app/src/main/java/com/mystique/rt/getmydrivercardapplcation/parsers/json/JsonParser.java
@@ -0,0 +1,11 @@
+package com.mystique.rt.getmydrivercardapplcation.parsers.json;
+
+import java.util.List;
+
+public interface JsonParser<T> {
+    List<T> fromJsonArray(String jsonString);
+
+    T fromJson(String jsonString);
+
+    String toJson(T object);
+}
