94678db2080d43e3b20c8cf60625ef62377552d9
t.filipova13@gmail.com <t.filipova13@gmail.com>
2018-10-29 15:14:26 +0200

    added functionality for async tasks

---
+++ Android/app/src/main/java/com/mystique/rt/getmydrivercardapplcation/async/AsyncSchedulerProvider.java
@@ -0,0 +1,30 @@
+package com.mystique.rt.getmydrivercardapplcation.async;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class AsyncSchedulerProvider implements SchedulerProvider {
+    private static SchedulerProvider instance;
+
+    private AsyncSchedulerProvider() {
+        // Empty private constructor for singleton
+    }
+
+    public static SchedulerProvider getInstance() {
+        if (instance == null) {
+            instance = new AsyncSchedulerProvider();
+        }
+        return instance;
+    }
+
+    @Override
+    public Scheduler background() {
+        return Schedulers.io();
+    }
+
+    @Override
+    public Scheduler ui() {
+        return AndroidSchedulers.mainThread();
+    }
+}
