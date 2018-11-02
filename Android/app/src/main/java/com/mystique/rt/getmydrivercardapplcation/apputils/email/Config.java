f813c4bfff3889e6a85406f1a6b997db96eca1d6
t.filipova13@gmail.com <t.filipova13@gmail.com>
2018-11-02 14:54:03 +0200

    added functionality for sending automatic email notifications

---
+++ Android/app/src/main/java/com/mystique/rt/getmydrivercardapplcation/apputils/email/Config.java
@@ -0,0 +1,6 @@
+package com.mystique.rt.getmydrivercardapplcation.apputils.email;
+
+public class Config {
+    public static final String EMAIL ="mystiquedrivercard@gmail.com";
+    public static final String PASSWORD ="aly74fbG8Izw3";
+}
